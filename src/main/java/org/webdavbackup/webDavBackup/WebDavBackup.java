9e3509c4d6487736448b0f30dd7aad9a1893e58c
Niklas <blis204@proton.me>
2024-09-09 15:56:42 +0200

    Removed test command

--- src/main/java/org/webdavbackup/webDavBackup/WebDavBackup.java
+++ src/main/java/org/webdavbackup/webDavBackup/WebDavBackup.java
@@ -2,14 +2,12 @@ package org.webdavbackup.webDavBackup;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.webdavbackup.webDavBackup.commands.BackupCommand;
-import org.webdavbackup.webDavBackup.commands.uwucommand;
 
 public final class WebDavBackup extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getCommand("uwu").setExecutor(new uwucommand());
         this.getCommand("backup").setExecutor(new BackupCommand(this));
     }
     @Override
